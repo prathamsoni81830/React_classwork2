27c5b779910b606b12bc41e46d2670d9d974d034
pratham soni <prathamsoni81830@gmail.com>
2024-05-26 21:23:16 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import "./App.css"
+function App() {
+  return (
+    <>
+    <div className="main">
+    <h1 style={{textAlign:"left", fontFamily:"sans-serif" ,marginLeft:"38px" , marginBottom:"30px"}}>Learn web development</h1>
+    <div style={{textAlign:"left", fontFamily:"sans-serif" ,marginBottom:"30px" ,fontSize:"17px"}}>
+    <p>Welcome to the MDN learning area. This set of articles aims to guide complete
+beginners to web development with all that they need to start coding websites.</p>
+</div>
+<div style={{textAlign:"left", fontFamily:"sans-serif" ,fontSize:"17px",marginBottom:"30px"}}>
+  <p>
+  The aim of this area of MDN is not to take you from "beginner" to "expert" but to
+take you from "beginner" to "comfortable." From there, you should be able to start
+making your way, learning from <a href="">the rest of MDN</a>, and other intermediate to
+advanced resources that assume a lot of previous knowledge.
+  </p>
+</div>
+<div style={{textAlign:"left", fontFamily:"sans-serif" ,fontSize:"17px"}}>
+  <p>
+  If you are a complete beginner, web development can be challenging -we will hold
+your hand and provide enough detail for you to feel comfortable and learn the topics
+properly. You should feel at home whether you are a student learning web
+development (on your own or as part of a class), a teacher looking for class
+materials, a hobbyist, or someone who just wants to understand more about how
+web technologies work.
+  </p>
+</div>
+</div>
+    </>
+  )
+}
+
+export default App
